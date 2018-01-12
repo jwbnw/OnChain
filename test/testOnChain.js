3bc798069dbdc0ef0bd88297e9af9f8956f4bd2e
jwbnw <james.w.belford@gmail.com>
2018-01-11 19:13:49 -0800

    Added a test case for setting a user Message

--- test/testOnChain.js
+++ test/testOnChain.js
@@ -1,5 +1,7 @@
+//Set our contract def
 var OnChain  = artifacts.require("./OnTheChain.sol");
 
+//open our contract in order to interact 
 contract('OnChain', function(interaction) {
 	it('should read the creator message',function(){
 
@@ -9,7 +11,16 @@ contract('OnChain', function(interaction) {
 			return instance.readCreatorWelcome.call();
 		}).then(function(result) {
 			creatorMessage = result.toString();
-			assert.equal(creatorMessage,"test","Contract is returning incorrect creator welcomw");
+			assert.equal(creatorMessage,"test","Contract is returning incorrect creator welcome see stack trace or use tuffle debugger");
+		});
+	});
+	
+	it("should allow user to send a message and author to the contract",function(){
+
+		return OnChain.deployed().then(function(instance){
+			return instance.setUserMessage("Test Message 1","Test User 1");
+		}).then(function(result){
+			assert.isOk(result,"User Message was not set see stack trace or use tuffle debugger")
 		});
 	});
 });
