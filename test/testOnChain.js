d36c13b75df9c6d3cfdb609ddf62ad17f5dbbc5c
jwbnw <james.w.belford@gmail.com>
2018-01-13 02:18:03 -0800

    added userMessage test

--- test/testOnChain.js
+++ test/testOnChain.js
@@ -14,13 +14,27 @@ contract('OnChain .js Tests', function(interaction) {
 			assert.equal(creatorMessage,"test","Contract is returning incorrect creator welcome see stack trace or use tuffle debugger");
 		});
 	});
-
 	it("should allow user to send a message and author to the contract",function(){
 
 		return OnChain.deployed().then(function(instance){
 			return instance.setUserMessage("Test Message 1","Test User 1");
 		}).then(function(result){
 			assert.isOk(result,"User Message was not set see stack trace or use tuffle debugger")
+		});
+	});
+	it("should call the user's message and time of message sent correctly",function(){
+
+		return OnChain.deployed().then(function(instance){
+			return instance.getUserMessage.call(interaction[0]);
+		}).then(function(result){
+
+			var result1 = result[0].toString();
+			var result2 = result[1].toString();
+			var result3 = new Date(result[2]*1000);
+
+			assert.equal(result1,"Test Message 1", "Message is not equal to user message");
+			assert.equal(result2,"Test User 1", "Message is not equal to user message");
+			assert.isNotNull(result3,"the timestamp did not register");
 		});
 	});
 });
